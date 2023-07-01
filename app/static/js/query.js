4d03d8f69e0e2950cbb5fcfa41add21f679f021a
Rick Spencer <rick@influxdata.com>
2023-07-01 09:27:58 -0400

    added line graph to render queries

--- app/static/js/query.js
+++ app/static/js/query.js
@@ -7,22 +7,79 @@ export var database = ""
 export var table = "*"
 
 
-export function runQuery(){
+export function runQuery() {
     var query = editor.getValue();
     var language = document.getElementById('languageSelect').value;
+
     $.ajax({
         url: '/api/query',  // URL of your Flask route
         type: 'POST',
         contentType: 'application/json',
-        data: JSON.stringify({query: query, language: language, database: currentDatabase}),
-        success: function(response) {
-            console.log(response);
-            // TODO: Handle the response data here
+        data: JSON.stringify({ query: query, language: language, database: currentDatabase }),
+        success: function (response) {
+            var data = JSON.parse(response);
+
+            var times = [];
+            var valueArrays = {};
+
+            // Populate the times and valueArrays
+            for (var i = 0; i < data.length; i++) {
+                times.push(new Date(data[i].time));
+                for (var key in data[i]) {
+                    if (key !== "time") {
+                        if (!(key in valueArrays)) {
+                            valueArrays[key] = [];
+                        }
+                        valueArrays[key].push(data[i][key]);
+                    }
+                }
+            }
+
+            var datasets = [];
+            for (var key in valueArrays) {
+                datasets.push({
+                    label: key,
+                    data: valueArrays[key],
+                    fill: false,
+                    borderColor: '#' + Math.floor(Math.random() * 16777215).toString(16),  // Generate a random color
+                    tension: 0.1
+                });
+            }
+
+            if (window.myChart instanceof Chart) {
+                window.myChart.destroy();
+            }
+            var ctx = document.getElementById('lineChart').getContext('2d');
+            window.myChart = new Chart(ctx, {
+                type: 'line',
+                data: {
+                    labels: times,
+                    datasets: datasets
+                },
+                options: {
+                    scales: {
+                        x: {
+                            type: 'time',
+                            time: {
+                                unit: 'day'
+                            },
+                            display: true,
+                            title: {
+                                display: true,
+                                text: 'Time'
+                            }
+                        },
+                        y: {
+                            beginAtZero: true
+                        }
+                    }
+                }
+            });
         },
-        error: function(error) {
+        error: function (error) {
             console.log(error);
         }
-    });   
+    });
 }
 
 export function buildQuery() {
